7645c966133c5551fbf4ebf53df2e633c09a1e27
barracudaking <barracudaking@gmail.com>
2021-06-24 11:10:43 +1000

    ?

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,8 +46,9 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE          := 0x00000000
+BOARD_RAMDISK_OFFSET       := 0x02000000
 TARGET_KERNEL_CLANG_COMPILE := true
-BOARD_KERNEL_IMAGE_NAME := Image-dtb
+BOARD_KERNEL_IMAGE_NAME := Image
 ifneq ($(BOARD_BOOT_HEADER_VERSION),3)
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm kpti=off
