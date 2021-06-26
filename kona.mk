e6ad2ca5180d3b670eef2c4963cda9ed0009ef3e
barracudaking <barracudaking@gmail.com>
2021-06-27 09:51:02 +1000

    NetWorkStack Overlays

--- kona.mk
+++ kona.mk
@@ -411,7 +411,8 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
-    WifiOverlay
+    WifiOverlay \
+    NetworkStackOverlay
 
 # WiFi Display
 PRODUCT_PACKAGES += \
